d88431724af94c247a9f4b7a292d931361d30076
Ben Lynch <bdlynch7@gmail.com>
2023-12-02 12:04:39 -0600

    finished setting up file structure

---
+++ day_01/solution_p1.js
@@ -0,0 +1,12 @@
+
+
+function adventor() {
+    // separate input by line break
+
+    // get first integer in string and last integer of string
+
+    // combine them into a single integer
+
+    // return sum of all values
+    return separatedLines
+}
